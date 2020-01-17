4ea689d44f1657721509a9de257da21ba1c780c0
Yui Ichioka <ichioka.yui@fixer.co.jp>
2020-01-17 18:29:04 +0900

    Add npm script (test:no-watch, test:coverage)

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -4,6 +4,6 @@ import { Logo } from "./Logo";
 
 test("renders learn react link", () => {
   const { getByText } = render(<Logo />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
